ce50468eeb7a67d3a0a1fadc3ea680c9603a183f
尤雨东 <root@ltpp.vip>
2025-02-09 21:37:04 +0800

    n

--- sh/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git init;
-git config core.ignorecase false;
-git remote add gitee git@gitee.com:ltpp-universe/china_identification_card.git;
-git remote add origin git@github.com:ltpp-universe/china_identification_card.git;
