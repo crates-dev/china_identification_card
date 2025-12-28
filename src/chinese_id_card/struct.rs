6c3a8a7edc5baab823e5f8643a47d62f80b65170
尤雨东 <root@ltpp.vip>
2025-12-28 11:13:52 +0800

    feat: v1.1.17

--- src/chinese_id_card/struct.rs
+++ src/chinese_id_card/struct.rs
@@ -2,4 +2,5 @@
 ///
 /// This structure encapsulates the logic for validating Chinese ID card numbers
 /// according to the official standards.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Default)]
 pub struct ChineseIdCard;
