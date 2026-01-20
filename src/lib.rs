362a8bda5c892e2897b443dda2726381a7956fbf
尤雨东 <root@ltpp.vip>
2026-01-20 23:13:08 +0800

    feat: v1.1.22

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,12 @@
 //!
 //! A Rust library for validating Chinese identification card numbers based on official rules and checksums.
 
-#[cfg(test)]
-mod test;
-
 pub(crate) mod r#const;
 pub(crate) mod r#impl;
 pub(crate) mod r#struct;
+
+#[cfg(test)]
+mod test;
 
 pub use r#struct::*;
 
